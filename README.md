dd6201ba1a73adb37346d3f61a052f4cda026b3f
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-19 11:22:37 -0700

    initial commit and getting started with my home-manager config

---
+++ README.md
@@ -0,0 +1,5 @@
+I've been wanting to play around with [nixos](https://nixos.org) for a while, so here it is.
+
+The goal here is to have a completely reproducible shell environment, ideally separated from the host OS, assuming it can have some sort of nixos layer installed on it. I would also like to have a completely reproducible host os, so it would be cool if it's somehow modular that I can kinda use like a cloud-init file or something and it brings up an entire environment for me with all of my dotfiles and configuration etc.
+
+
---
+++ home.nix
@@ -0,0 +1,115 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "kitchen";
+  home.homeDirectory = "/home/kitchen";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.11"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+    pkgs.btop
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. If you don't want to manage your shell through Home
+  # Manager then you have to manually source 'hm-session-vars.sh' located at
+  # either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/kitchen/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  programs.zsh = {
+    enable = true;
+
+    oh-my-zsh = {
+      enable = true;
+      plugins = ["git" "thefuck" "zoxide" ];
+      theme = "robbyrussell";
+      # this option is only from a newer version of zoxide oh-my-zsh plugin
+      extraConfig = "export ZOXIDE_CMD_OVERRIDE=cd";
+    };
+  };
+
+  programs.neovim = {
+    enable = true;
+  };
+
+  programs.tmux = {
+    enable = true;
+  };
+
+  programs.thefuck = {
+    enable = true;
+  };
+  
+  programs.git = {
+    enable = true;
+    userName = "Jeremy Kitchen";
+    userEmail = "kitchen@kitchen.io";
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+    };
+  };
+
+  programs.zoxide = {
+    enable = true;
+    # to be replaced with oh-my-zsh plugin config option when omz upgrades
+    options = [
+      "--cmd cd"
+    ];
+  };
+}
