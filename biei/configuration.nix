e003a3772c4b51cf2fd641b5a43cf1484b3fbc52
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-21 13:52:12 -0700

    add authorized key for shihoro to biei config

--- biei/configuration.nix
+++ biei/configuration.nix
@@ -67,6 +67,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK20Mz6/MC7C9IRr5YrIwH34o2A7cUppyFMLT3k5jh9f kitchen@taipei.kitchen.io"
       # taipei ecdsa key
       "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBPP1SUY0xXafvoWwDuNbNqq4ut09OMGFLBkzWdgxFCCG3lfVVSDnJYG1R4WgLMhESCQkEE8K+n1LnH8AJg0n25k= kitchen@taipei.kitchen.io"
+      # shihoro
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmG1/Bs2oKqYa9UNYisZT+armcZzQRsa1mUziAp+Dsh kitchen@shihoro"
     ];
   };
 
