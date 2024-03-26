edfbb5580ca7c10c3b83d881fe09dffe61cba6dc
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-26 12:25:32 -0700

    hide .git directory

--- modules/neovim/lua/plugins/neotree.lua
+++ modules/neovim/lua/plugins/neotree.lua
@@ -5,6 +5,9 @@ return {
       local filtered_items = opts.filesystem.filtered_items or {}
       filtered_items["hide_dotfiles"] = false
       filtered_items["hide_gitignored"] = true
+      filtered_items["hide_by_name"] = {
+        ".git"
+      }
 
       opts.filesystem["filtered_items"] = filtered_items
     end,
