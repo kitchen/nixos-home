165f084093ef01673ef775b2f981b6ecb87c29e4
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-27 21:12:06 -0700

    enable rose-pine theme for btop
    thought about just copying the theme file over and to be honest probably
    should have, but I like playing with things so here it is.
    
    because I didn't want to manage the entire btop/plugins directory
    as this whole repo, and I couldn't figure out how to use a relative path
    to the theme, I decided to just grab the one file, which is somehow
    (yea, I really want to dig more into that) limited to just the one file
    by ... adding a path to the fetchFromGithub return? I really need to
    look into how this works haha. I'm actually thinking fetchFromGithub
    just returns a path to the ... derivation? hmm

--- modules/btop.nix
+++ modules/btop.nix
@@ -1,4 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 {
-  programs.btop.enable = true;
-}
+  programs.btop = {
+    enable = true;
+    settings.color_theme = "rose-pine";
+  };
+
+  # HT https://stackoverflow.com/questions/53658303/fetchfromgithub-filter-down-and-use-as-environment-etc-file-source
+  xdg.configFile."btop/themes/rose-pine.theme" = {
+    source = 
+      pkgs.fetchFromGitHub {
+        owner = "rose-pine";
+        repo = "btop";
+        rev = "6d6abdc0a8c8bcd3b056d9fe3256cfbe7e359312";
+        sha256 = "sha256-sShQYfsyR5mq/e+pjeIsFzVZv3tCpQEdGC9bnTKlQ5c=";
+      } + "/rose-pine.theme";
+    };
+  }
