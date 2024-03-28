05fab218b40ac71cfd51bb6320a473affe1ff9c2
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-27 21:54:06 -0700

    install and enable docker

--- biei/configuration.nix
+++ biei/configuration.nix
@@ -53,7 +53,7 @@
   users.users.kitchen = {
     isNormalUser = true;
     description = "Jeremy Kitchen";
-    extraGroups = [ "networkmanager" "wheel" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
@@ -76,14 +76,6 @@
     neofetch
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
@@ -92,11 +84,7 @@
 
   services.tailscale.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  virtualisation.docker.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
