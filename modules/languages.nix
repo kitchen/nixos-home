121db9d17e13bc83dd0c47d1ad531d924c73c48a
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-31 15:58:32 -0700

    install nodejs
    I'd like to play around with the devcontainer-cli node package to be able to do nicely contained development and builds with zmk so let's install nodejs here!
    
    From there I should be able to use `npx` to run devcontainer-cli things and keep the node deps nicely contained as well. I think :X

--- modules/languages.nix
+++ modules/languages.nix
@@ -3,6 +3,7 @@
   # many/most of these don't have extra config but I want to leave room for them to later
   imports = [
     ./languages/golang.nix
+    ./languages/nodejs.nix
     ./languages/rust.nix
     ./languages/terraform.nix
   ];
