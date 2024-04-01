4f3bb4f459f2aa63bc81896747fd220a4faf4c1c
Jeremy Kitchen <kitchen@kitchen.io>
2024-03-31 20:19:12 -0700

    disable all incoming traffic
    except the tailscale interface, that's wide open!

--- systems/README.md
+++ systems/README.md
@@ -1,3 +1,7 @@
 these are still all copied around manually and such and I attempt to keep them in sync.
 
 I'll eventually figure out how to something something with these but for now they're mostly just a backup and I try to edit here first and copy things over rather than do it live and copy back
+
+# current status
+
+as of writing, the way I'm using these is I am copying all of the `*.nix` files from this directory and `<machine>/configuration.nix` into `/etc/nixos/` and using `nixos-rebuild switch` to apply them. Seems to work fine.
--- systems/security.nix
+++ systems/security.nix
@@ -1,21 +1,29 @@
 { config, lib, pkgs, ... }:
 {
   # TODO: firewall configuration
+  # basically what I want here is everything external is just banned, but anything coming in via tailscale is fine.
+  #
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
+  networking.firewall = {
+    enable = true;
 
-  security.sudo.wheelNeedsPassword = false;
+    trustedInterfaces = [ "tailscale0" ];
+  };
 
   services.openssh = {
     enable = true;
+    openFirewall = false;
     settings = {
       PermitRootLogin = "no";
-      PasswordAuthentication = "no";
+      PasswordAuthentication = false;
     };
   };
+
+  security.sudo.wheelNeedsPassword = false;
 
   services.tailscale.enable = true;
 }
